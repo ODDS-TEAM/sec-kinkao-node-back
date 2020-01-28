591d10b8321551764cf61c6aae9fdbda6ba030d2
chompusama <cpu@odds.team>
2020-01-28 18:24:50 +0700

    add api for update state that is cc and cf

--- api/routes/foodOrderMerchantRoutes.js
+++ api/routes/foodOrderMerchantRoutes.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 _ = require("underscore")
 
 const orderActivitiesCollection = require("../models/orderActivitiesModel");
+const dayMenusCollection = require("../models/dayMenusModel")
 
 //show all order for merchant
 router.get("/:merchantId", (req, res, next) => {
@@ -52,48 +53,115 @@ router.get("/:merchantId", (req, res, next) => {
 //show order detail for merchant
 router.get("/detail/:orderId", (req, res, next) => {
 
-  orderActivitiesCollection.findOne({ _id: req.params.orderId })
-    .exec()
-    .then(docs => {
-      if (docs != '') {
-        res.status(200).json(docs)
-      }
-      else {
-        res.status(401).json({
-            message: "empty"
+    orderActivitiesCollection.findOne({ _id: req.params.orderId })
+        .exec()
+        .then(docs => {
+            if (docs != '') {
+                res.status(200).json(docs)
+            }
+            else {
+                res.status(401).json({
+                    message: "empty"
+                })
+            }
         })
-      }
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                message: err
+            })
+        });
+
+});
+
+
+//when merchant want to accept an order
+router.post("/confirm/:orderId", (req, res, next) => {
+    var dayMenuIdArray;
+    orderActivitiesCollection.aggregate([
+        {
+          $match: { _id: req.params.orderId }
+        },
+        {
+          $project: { 
+            _id: 0,
+            dayMenuId: '$items.dayMenuId'
+          }
+        }
+      ]).exec((err, result) => {
+        if (err) {
+            res.status(401).json({
+                message: err
+            })
+        }
+        else {
+            if (result != '') {
+                // res.status(200).json(result[0].dayMenuId)
+                dayMenuIdArray = result[0].dayMenuId
+                console.log(dayMenuIdArray)
+                dayMenusCollection.update({ _id: { $in: dayMenuIdArray } }, {
+                    $inc: {
+                        foodLeft: -1
+                    }
+                }, function (err, docs) {
+                    if (err) {
+                        res.status(401).json({
+                            message: err
+                        })
+                    }
+                    else {
+                        res.status(200).json({
+                            message: "foodLeft decreased"
+                        })
+                    }
+                });
+            }
+            else {
+                res.status(401).json({
+                    message: 'empyty'
+                })
+            }
+        }
     })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-          message: err
-      })
+
+    orderActivitiesCollection.updateOne({ _id: req.params.orderId }, {
+        $set: {
+            state: 'cf'
+        }
+    }, function (err, docs) {
+        if (err) {
+            res.status(401).json({
+                message: err
+            })
+        }
+        else {
+            console.log('state updated to confirm')
+        }
     });
 
 });
 
-// router.post("/cancel/:orderId", (req, res, next) => {
 
-//     orderActivitiesCollection.find({ _id: req.params.orderId }).sort({ _id: -1 })
-//       .exec()
-//       .then(docs => {
-//         if (docs != '') {
-//           res.status(200).json(docs)
-//         }
-//         else {
-//           res.status(401).json({
-//               message: "empty"
-//           })
-//         }
-//       })
-//       .catch(err => {
-//         console.log(err);
-//         res.status(500).json({
-//             message: err
-//         })
-//       });
-  
-//   });
+//when merchant want to reject an order
+router.post("/cancel/:orderId", (req, res, next) => {
+
+    orderActivitiesCollection.updateOne({ _id: req.params.orderId }, {
+        $set: {
+            state: 'cc'
+        }
+    }, function (err, docs) {
+        if (err) {
+            res.status(401).json({
+                message: err
+            })
+        }
+        else {
+            res.status(200).json({
+                message: "state updated to canceled (cc)"
+            })
+        }
+    });
+
+});
 
 module.exports = router;
--- api/routes/scheduleRoutes.js
+++ api/routes/scheduleRoutes.js
@@ -7,7 +7,8 @@ _ = require("underscore")
 const dayMenusCollection = require("../models/dayMenusModel");
 
 router.post("/random", (req, res, next) => {
-    dayMenusCollection.insertMany(req.body)
+    var dayMenu = new dayMenusCollection(_.extend({ _id: new mongoose.Types.ObjectId() }, req.body));
+    dayMenusCollection.insertMany(dayMenu)
         .then(result => {
             res.status(201).json(result)
         })
@@ -16,6 +17,28 @@ router.post("/random", (req, res, next) => {
                 message: err
             })
         });
+});
+
+router.get("/:merchantId", (req, res, next) => {
+    dayMenusCollection.find({ merchantId: req.params.merchantId })
+        .exec()
+        .then(docs => {
+            if (docs != '') {
+                res.status(200).json(docs)
+            }
+            else {
+                res.status(401).json({
+                    message: "empty"
+                })
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                message: err
+            })
+        });
+
 });
 
 module.exports = router;
